e413d03e29c2a7c8b5c60188c88298ee60d5127c
JadivaM <jmont261@fiu.edu>
2021-02-22 22:31:37 -0500

    set up scraping route

--- server.js
+++ server.js
@@ -5,14 +5,15 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const path = require('path');
 const app = express();
+const newsRouter = require('./routes/');
 
-// JUST FOR DEMO PURPOSES, PUT YOUR ACTUAL API CODE HERE
+app.use(newsRouter);
+
 app.get('/api/demo', (request, response) => {
   response.json({
     message: 'Hello from server.js'
   });
 });
-// END DEMO
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
